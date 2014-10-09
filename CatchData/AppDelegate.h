22f77f4e07e5939d71a03a0e1500c71657f142bc
chrischensy <chenshuaiyu@csyAir.local>
2014-10-09 14:44:24 +0800

    update
    update

---
+++ CatchData/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CatchData
+//
+//  Created by csy on 14-10-9.
+//  Copyright (c) 2014年 ssmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CatchData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CatchData
+//
+//  Created by csy on 14-10-9.
+//  Copyright (c) 2014年 ssmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
